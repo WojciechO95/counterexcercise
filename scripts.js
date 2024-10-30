03301f2395d3484e917cf09e7cdb1056226e10e8
WojciechO95 <wojciech.opitz@bayer.com>
2024-10-30 13:04:18 +0100

    first commit

---
+++ scripts.js
@@ -0,0 +1,15 @@
+const endDate = new Date("Dec 24, 2024, 16:00:00").getTime();
+
+const callback = () => {
+  const dateNow = new Date().getTime();
+  let timeLeft = endDate - dateNow;
+  let days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+  document.getElementById("countdown").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+};
+
+window.setInterval(callback, 1000);
